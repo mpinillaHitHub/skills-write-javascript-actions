1eecf2202e6e596221d7ceda6d4ec757711e754d
mpinillaHitHub <mpinillarpagos@gmail.com>
2026-09-08 14:13:38 +0000

    Add Dad Joke action source files

--- src/joke.js
+++ src/joke.js
@@ -1,4 +1,4 @@
-const request = require("request-promise");
+import request from "request-promise";
 
 const options = {
       method: "GET",
@@ -10,9 +10,8 @@ const options = {
             json: true,
         };
 
-        async function getJoke() {
+        export async function getJoke()  {
               const res = await request(options);
                 return res.joke;
         }
-
-        module.exports = getJoke;
+    
